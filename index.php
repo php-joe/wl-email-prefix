e83a50262c69d39aa9db9de3dc32968533333fdd
php-joe <joseph@schnauzerdesign.com>
2015-09-20 15:41:22 -0600

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Arranging your array</title>
+</head>
+<body>
+<h2>Problem 2: Input Email address, then display the pre-fix.</h2>
+  <form method="POST">
+    <input type="text" placeholder="Email Address" name="email" />
+      <input type="submit"/>
+    </form>
+<?php require_once('mylib.php');
+firstPartEmail();
+?>
+
+</body>
+</html>
